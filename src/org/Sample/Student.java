49ceb0e63827dc72162149c616b9f8ea78eeb465
Bernath <bernathvelmurugan2503@gmail.com>
2021-12-22 15:54:42 +0530

    Bernath push the code

---
+++ src/org/Sample/Student.java
@@ -0,0 +1,49 @@
+package org.Sample;
+
+public class Student {
+	
+	public static void main(String[] args) {
+		
+
+		int a[] = new int[6];
+		
+		a[0]=10;
+		a[1]=20;
+		a[2]=10;
+		a[3]=30;
+		a[4]=40;
+		a[5]=30;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
